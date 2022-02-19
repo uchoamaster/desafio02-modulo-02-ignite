dcb273b9b54d8e6e5a3f258d338a37396c5a6704
Carlos Uchoa <carlosjuchoa@hotmail.com>
2022-02-19 09:20:55 -0300

    finalizando a parte de de edicao dos componentes

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,46 +1,92 @@
-import { Component } from 'react';
-import { FiEdit3, FiTrash } from 'react-icons/fi';
+
 import { useState } from 'react';
+import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
 import api from '../../services/api';
 
-export default function Food({ food }){
-const { available } = food;
-const [ isAvailable, setIsAvailable ] = useState(available)
-
-const toggleAvailable = async () => {
-  await api.put(`/foods/${food.id}`, {
-    ...food,
-    available: !isAvailable,
-  });
-
-  setIsAvailable({ !isAvailable });
+interface IFood {
+  id:number;
+  name:string;
+  description:string;
+  price:string;
+  available:boolean;
+  image:string;
 }
 
-  return (
-
-  )
+interface FoodProps {
+  food: IFood;
+  handleEditFood: (food:IFood) => void
+  handleDelete: (id:number) => void
 }
 
+export default function Food({food, handleEditFood, handleDelete}: FoodProps) {
+  const { available } = food;
+  const [isAvailable, setIsAvailable] = useState(available)
 
-class Food extends Component {
+  const toggleAvailable = async () => {
+    await api.put(`/foods/${food.id}`, {
+      ...food,
+      available: !isAvailable,
+    });
 
+    setIsAvailable(!isAvailable);
+  }
 
-
-
-  setEditingFood = () => {
-    const { food, handleEditFood } = this.props;
-
+  const setEditingFood = () => {
     handleEditFood(food);
   }
 
-  render() {
-    const { isAvailable } = this.state;
-    const { food, handleDelete } = this.props;
+  return (
+    <Container available={isAvailable}>
+      <header>
+        <img src={food.image} alt={food.name} />
+      </header>
+      <section className="body">
+        <h2>{food.name}</h2>
+        <p>{food.description}</p>
+        <p className="price">
+          R$ <b>{food.price}</b>
+        </p>
+      </section>
+      <section className="footer">
+        <div className="icon-container">
+          <button
+            type="button"
+            className="icon"
+            onClick={setEditingFood}
+            data-testid={`edit-food-${food.id}`}
+          >
+            <FiEdit3 size={20} />
+          </button>
 
-    
-  }
-};
+          <button
+            type="button"
+            className="icon"
+            onClick={() => handleDelete(food.id)}
+            data-testid={`remove-food-${food.id}`}
+          >
+            <FiTrash size={20} />
+          </button>
+        </div>
 
-export default Food;
+        <div className="availability-container">
+          <p>{isAvailable ? 'Disponível' : 'Indisponível'}</p>
+
+          <label htmlFor={`available-switch-${food.id}`} className="switch">
+            <input
+              id={`available-switch-${food.id}`}
+              type="checkbox"
+              checked={isAvailable}
+              onChange={toggleAvailable}
+              data-testid={`change-status-food-${food.id}`}
+            />
+            <span className="slider" />
+          </label>
+        </div>
+      </section>
+    </Container>
+  );
+
+}
+
